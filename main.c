2cf29c386cff236aa17c489676e076c5941ae88a
SumitDesai-21 <sumit.desai3433@gmail.com>
2026-04-07 13:28:40 +0530

    Fixed infinite loop

--- main.c
+++ main.c
@@ -2,7 +2,7 @@
 #include "add.h"
 #include "sub.h"
 #include "mult_div.h"
-
+#include <stdbool.h>
 int main() {
 	int a, b;
 	char operation;
@@ -10,23 +10,29 @@ int main() {
 	scanf("%d%d", &a, &b);
 	printf("Enter operation(+, -, * or /): \n");
 	scanf(" %c", &operation);
-	while (true) {
+	bool flag = true;
+	while (flag) {
 		switch(operation) {
 			case '+':
 				add(a, b);
+				flag = false;
 				break;
 			case '-':
 				sub(a, b);
+				flag = false;
 				break;
 			case '*':
 				mult(a, b);
+				flag = false;
 				break;
 			case '/':
 				div(a, b);
+				flag = false;
 				break;
 			default:
 				printf("Enter valid input\n");
 		}
+
 	}
 	return 0;
 }
